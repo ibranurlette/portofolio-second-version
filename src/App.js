3f2a707c605e010f62db17c41583d541173dddd8
Ibra Nurlette <sjsdev02@gmail.com>
2022-07-06 14:33:33 +0700

    fix introduction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css";
 import {
   IoLogoLinkedin,
   IoLogoGithub,
@@ -12,35 +13,55 @@ import {
 
 function App() {
   return (
-    <div className="bg-sky-900">
+    <div>
       {/* first section */}
       <div className="pt-8 pb-8">
         <div className="mb-5 text-center ">
-          <h1 className="text-white">Hello i'm</h1>
+          <h1 className="text-white">Hello I'm</h1>
           <h2 className="text-3xl font-bold text-white">Ibra Nurlette</h2>
           <h1 className="text-white">Frontend Developer</h1>
         </div>
         <div className="mb-8 text-center	">
-          <button className="rounded-md text-sky-500 border-2 border-blue-500 p-3 mr-4">
-            Download Cv
+          <button className="rounded-md text-sky-400 border-2 border-sky-400 p-3 mr-4 font-medium">
+            Download CV
           </button>
-          <button className="rounded-md bg-sky-400 p-3">Let's Talk </button>
+          <button className="rounded-md bg-sky-400 border-2 border-sky-400 p-3 font-medium">
+            Let's Talk{" "}
+          </button>
         </div>
         <div className="flex justify-around items-end ">
           <div>
-            <IoLogoLinkedin className="text-white" size="1.5em" />
-            <IoLogoGithub className="text-white mt-2 mb-2" size="1.5em" />
-            <IoLogoInstagram className="text-white" size="1.5em" />
+            <a
+              href="https://www.linkedin.com/in/ibra-nurlette/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoLinkedin className="text-sky-400" size="1.5em" />
+            </a>
+            <a
+              href="https://github.com/ibranurlette"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoGithub className="text-sky-400 mt-4 mb-4" size="1.5em" />
+            </a>
+            <a
+              href="https://www.instagram.com/ibra_nurlette/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoInstagram className="text-sky-400" size="1.5em" />
+            </a>
           </div>
-          <div className="bg-gradient-to-r from-cyan-500 to-blue-500 w-72 p-2 rounded-t-[150px]">
+          <div className="bg-sky-400 bg-image w-72 rounded-t-[150px]">
             <img
-              src={require("./assets/me.jpg")}
+              src={require("./assets/me2.png")}
               alt="me"
-              className=" mx-auto rounded-t-[150px]"
-            ></img>
+              className="rounded-t-[150px]"
+            />
           </div>
           <div>
-            <h2 className="rotate-90 mb-8 text-white"> Scroll down</h2>
+            <h2 className="rotate-90 mb-8 text-sky-400"> Scroll down</h2>
           </div>
         </div>
       </div>
@@ -57,21 +78,21 @@ function App() {
         </div>
         <div className="">
           <div className="flex justify-start mb-5 text-center">
-            <div className="bg-sky-500 p-2 mr-5 rounded-lg">
+            <div className="bg-sky-400 p-2 mr-5 rounded-lg">
               <p>
                 <IoMedal className="text-white mx-auto" size="1.5em" />
               </p>
               <p className="mt-2 mb-2">Experience</p>
               <p>2+ Years Working</p>
             </div>
-            <div className="bg-sky-500 p-2 mr-5 rounded-lg">
+            <div className="bg-sky-400 p-2 mr-5 rounded-lg">
               <p>
                 <IoPeople className="text-white mx-auto" size="1.5em" />
               </p>
               <p className="mt-2 mb-2">Clients</p>
               <p>300+ Worldwide</p>
             </div>
-            <div className="bg-sky-500 p-2 rounded-lg">
+            <div className="bg-sky-400 p-2 rounded-lg">
               <p>
                 <IoCreate className="text-white mx-auto" size="1.5em" />
               </p>
@@ -101,7 +122,7 @@ function App() {
         My Experience
       </h2>
       <div className="m-10 flex justify-center">
-        <div className="bg-sky-500 py-12 px-10 mr-7 w-[32rem] rounded-[20px]">
+        <div className="bg-sky-400 py-12 px-10 mr-7 w-[32rem] rounded-[20px]">
           <h1 className="text-2xl mb-8 font-bold">Frontend developer</h1>
           <div className="flex justify-between ">
             <div className="">
@@ -164,7 +185,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="bg-sky-500 py-12 px-10 w-[32rem] rounded-[20px]">
+        <div className="bg-sky-400 py-12 px-10 w-[32rem] rounded-[20px]">
           <h1 className="text-2xl mb-8 font-bold">Backend developer</h1>
           <div className="flex justify-between ">
             <div className="">
@@ -215,7 +236,7 @@ function App() {
       <h1 className="text-white text-center">My Recent Work</h1>
       <h2 className="text-3xl font-bold text-white text-center">Portfolio</h2>
       <div className="m-10 grid grid-cols-3 mx-80 ">
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -227,7 +248,7 @@ function App() {
           </button>
           <button className="rounded-md bg-sky-400 p-3">Live Demo</button>
         </div>
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -239,7 +260,7 @@ function App() {
           </button>
           <button className="rounded-md bg-sky-400 p-3">Live Demo</button>
         </div>
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -251,7 +272,7 @@ function App() {
           </button>
           <button className="rounded-md bg-sky-400 p-3">Live Demo</button>
         </div>
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5 mt-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5 mt-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -263,7 +284,7 @@ function App() {
           </button>
           <button className="rounded-md bg-sky-400 p-3">Live Demo</button>
         </div>
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5 mt-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5 mt-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -275,7 +296,7 @@ function App() {
           </button>
           <button className="rounded-md bg-sky-400 p-3">Live Demo</button>
         </div>
-        <div className="bg-sky-500 p-5 rounded-[20px]	mr-5 mt-5">
+        <div className="bg-sky-400 p-5 rounded-[20px]	mr-5 mt-5">
           <img
             src={require("./assets/me.jpg")}
             alt="me"
@@ -295,7 +316,7 @@ function App() {
       <div className="m-10 flex justify-center">
         <div className="">
           <div className="flex justify-start mb-5 text-center">
-            <div className="bg-sky-500 px-2 py-4 mr-5 rounded-lg w-56">
+            <div className="bg-sky-400 px-2 py-4 mr-5 rounded-lg w-56">
               <p>
                 <IoMail className="text-white mx-auto" size="1.5em" />
               </p>
@@ -303,7 +324,7 @@ function App() {
               <p className="font-light">ibraputra843@gmail.com</p>
               <p>Send a message</p>
             </div>
-            <div className="bg-sky-500 px-2 mr-5 py-4 rounded-lg w-56">
+            <div className="bg-sky-400 px-2 mr-5 py-4 rounded-lg w-56">
               <p>
                 <IoLogoLinkedin className="text-white mx-auto" size="1.5em" />
               </p>
@@ -311,7 +332,7 @@ function App() {
               <p className="font-light">Ibra_Nurlette</p>
               <p>Send a message</p>
             </div>
-            <div className="bg-sky-500 px-2 py-4 rounded-lg w-56">
+            <div className="bg-sky-400 px-2 py-4 rounded-lg w-56">
               <p>
                 <IoLogoInstagram className="text-white mx-auto" size="1.5em" />
               </p>
